4bc79fd4d5f369ccbceba49bab8f42ec8bb4c90a
DN0149 <jarcem@sina.com>
2019-03-18 13:19:29 +0800

    2019.3.18

--- src/main/java/org/jarcem/http/server/WebServer.java
+++ src/main/java/org/jarcem/http/server/WebServer.java
@@ -5,10 +5,9 @@ package org.jarcem.http.server;
   Purpose: 
 */
 
-import org.jarcem.recognizer.Recognizer;
-import org.jarcem.recognizer.api.SpeechResult;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -23,29 +22,30 @@ public class WebServer {
 //            WebResponseServer.updataMsg(speechResult.getHypothesis().getBytes());
 //            Server.updateMsg(speechResult.getHypothesis().getBytes());
 //        }
-        Scanner sn = new Scanner(System.in);
-        while (true){
-            Server.updateMsg(sn.next().getBytes());
-        }
     }
-
     static class Server{
         static ServerSocket serverSocket = null;
         static Socket socket = null;
         static OutputStream os = null;
 //        static InputStream is = null;
         static void start() throws IOException {
-            serverSocket = new ServerSocket(8080);
-            while (socket ==null){
+            serverSocket = new ServerSocket(808);
+            while (true) {
                 socket = serverSocket.accept();
                 System.out.println("connected");
+                updataMsg();
             }
         }
-
-        static void updateMsg(byte[] message) throws IOException {
+        static void updataMsg() throws IOException {
             if (socket != null){
                 os = socket.getOutputStream();
-                os.write(message);
+                os.write("HTTP/1.1 200 OK\r\n".getBytes());
+                os.write("Content-Type:text/html;charset=utf-8\r\n".getBytes());
+                os.write("Content-Length:38\r\n".getBytes());
+                os.write("Server:gybs\r\n".getBytes());
+                os.write("\r\n".getBytes());
+                os.write("<h1>hello!</h1>".getBytes());
+                os.write("<h3>HTTP服务器!</h3>".getBytes("utf-8"));
                 os.close();
             }
         }
